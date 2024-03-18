c2ba65ce54f36d68559821b4cbe58d81331f4e63
OlaBekkevold <ola.k.bekkevold@gmail.com>
2024-03-18 13:42:41 +0100

    Add a way to ask and answer correctly on questions

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Hello, World!");
+const questions = [
+    {
+        question: "Hvor mange folk består Mapuche folket av?",
+        options: ["1,9 millioner", "1,7 millioner", "2 millioner", "1,5 millioner"],
+        answer: "1,9 millioner"
+    },
+    {
+        question: "What is the capital of Germany?",
+        options: ["Paris", "London", "Berlin", "Rome"],
+        answer: "Berlin"
+    },
+    {
+        question: "What is the capital of Italy?",
+        options: ["Paris", "London", "Berlin", "Rome"],
+        answer: "Rome"
+    },
+    {
+        question: "What is the capital of England?",
+        options: ["Paris", "London", "Berlin", "Rome"],
+        answer: "London"
+    }
+];
+
+let score = 0;
+function increaseScore() {
+    score++;
+    console.log("Riktig svar! Din score er nå: " + score);
+}
+
+
+let answer = "";
+
+answer = prompt(questions[0].question + "Svaralternativ: " + questions[0].options + "Skriv inn ditt svaralternativ");
+
+
+if (answer === questions[0].answer) {
+    increaseScore();
+
+} else {
+    console.log("Feil svar! Din score er: " + score);
+}
+
+
+
+
